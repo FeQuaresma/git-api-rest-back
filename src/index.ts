86a8c74696f5f4c8da35f86e93182e2470e98f8a
Felipe Quaresma Damasceno <felipequada@gmail.com>
2022-12-25 15:06:11 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({ auth: process.env.GBKEY });
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/api/users", async (req: Request, res: Response) => {
+  const since = req.query.since;
+  const per_page = req.query.per_page;
+  const usersList = await octokit.request(
+    `/users?since=${since}&per_page=${per_page}`
+  );
+  res.json(usersList.data);
+});
+
+app.get("/api/users/:username/details", async (req: Request, res: Response) => {
+  const username = req.params.username;
+  const userDetails = await octokit.request(
+    `https://api.github.com/users/${username}`
+  );
+  console.log(userDetails.data)
+  res.json(userDetails.data);
+});
+
+app.get("/api/users/:username/repos", async (req: Request, res: Response) => {
+  const username = req.params.username;
+  const userRepos = await octokit.request(
+    `https://api.github.com/users/${username}/repos`
+  );
+  res.json(userRepos.data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
